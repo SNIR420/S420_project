2c03bf6eaa24836e2f8ffce1851fde73358f852e
SNIR420 <botnum57@gmail.com>
2023-02-03 16:19:14 +0100

    added stuff

--- rollme.h
+++ rollme.h
@@ -1,15 +1,5 @@
-class Simulateur{
+class CRollme {
 
     public:
-        explicit Simulateur() ;
-        double getTwa();
-        double getTws();
-        double getAngleBome();
-        double getSpeed();
-        void setAngleVentJeu(double twa);
-        void setTws(double tws);
-        void setWave(int force); // échelle de beaufort
-
-    public slots:
-        void info(const QString& src, const QString& msg ) ;
+        
 } ;
