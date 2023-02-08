f5a2e7e814b465a06a8fc586eddfe1a943bd40d3
SNIR420 <botnum57@gmail.com>
2023-02-08 09:02:56 +0100

    ajout de trucs

--- rollme.h
+++ rollme.h
@@ -1,3 +1,24 @@
+/*
+point O = centre du repère
+point B = centre du bateau
+point C = avant du bateau 
+point A = arrière du bateau
+point D = droite du bateau
+point E = gauche du bateau
+repère de l'espace XYZ
+repère du bateau xyz
+angle θ (thêta) entre X et x (ou Y et y)
+
+OB = (X ; Y)
+OC = (X + L/2 sinθ ; Y + L/2 cosθ)
+OA = (X - L/2 sinθ ; Y - L/2 cosθ)
+OE = (X - e/2 cosθ ; Y + e/2 sinθ)
+OD = (X + e/2 cosθ ; Y - e/2 sinθ)
+*/
+
+#define envergure e
+#define Longueur L
+
 class CRollme {
 
     public:
