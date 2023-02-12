a3664fe8e9d7e1180c32aecccd64cd92bdb5694d
SNIR420 <botnum57@gmail.com>
2023-02-12 10:56:06 +0100

    Mise en place du client Modbus sur l'IHM avec l'envoi et la lecture sur une seule table d'un registre Modbus

--- S420_IHM/ihm.cpp
+++ S420_IHM/ihm.cpp
@@ -1,12 +1,94 @@
 #include "ihm.h"
 #include "./ui_ihm.h"
 
+#include <QModbusDataUnit>
+#include <QModbusTcpClient>
+#include <QModbusReply>
+#include <QDebug>
+
 IHM::IHM(QWidget *parent): QWidget(parent), ui(new Ui::IHM)
 {
-   ui->setupUi(this);
+    ui->setupUi(this);
+
+    // Connecter le bouton pushButton avec la fonction sendValues
+    connect(ui->pushButton, &QPushButton::clicked, this, &IHM::sendValues);
+    connect(ui->pushButton_2, &QPushButton::clicked, this, &IHM::readValues);
+    // Initialiser le client Modbus
+    modbusDevice = new QModbusTcpClient(this);
+    modbusDevice->setConnectionParameter(QModbusDevice::NetworkAddressParameter, "127.0.0.1");
+    modbusDevice->setConnectionParameter(QModbusDevice::NetworkPortParameter, 502);
+    modbusDevice->setTimeout(1000);
+    modbusDevice->setNumberOfRetries(3);
+    modbusDevice->connectDevice();
 }
 
 IHM::~IHM()
 {
+    delete ui;
 }
 
+void IHM::sendValues()
+{
+    int value = ui->doubleSpinBox_2->value();
+
+    if (!modbusDevice)
+        return;
+
+    QModbusDataUnit writeUnit(QModbusDataUnit::HoldingRegisters, 16, 1);
+    writeUnit.setValue(0, value);
+
+    auto *reply = modbusDevice->sendWriteRequest(writeUnit, 1);
+
+    if (!reply) {
+        QMessageBox::critical(this, tr("Error"), tr("Write error: ") + modbusDevice->errorString());
+    } else {
+        if (!reply->isFinished()) {
+            connect(reply, &QModbusReply::finished, this, [this, reply]() {
+                if (reply->error() == QModbusDevice::ProtocolError) {
+                    QMessageBox::critical(this, tr("Error"), tr("Write error: ") + reply->errorString());
+                } else if (reply->error() != QModbusDevice::NoError) {
+                    QMessageBox::critical(this, tr("Error"), tr("Write error: ") + reply->errorString());
+                } else {
+                    QMessageBox::information(this, tr("Success"), tr("Write success"));
+                }
+
+                reply->deleteLater();
+            });
+        } else {
+            reply->deleteLater();
+        }
+    }
+}
+
+void IHM::readValues()
+{
+    const auto startAddress = 15;
+
+    if (!modbusDevice)
+        return;
+    QModbusDataUnit readUnit(QModbusDataUnit::HoldingRegisters, 16, 1);
+    auto reply = modbusDevice->sendReadRequest(readUnit, 1);
+    if (reply) {
+        if (!reply->isFinished()) {
+            connect(reply, &QModbusReply::finished, this, [this, reply]() {
+                if (reply->error() == QModbusDevice::NoError) {
+                    const auto registers = reply->result().values();
+                    if (registers.size() == 1) {
+                        const auto value = registers[0];
+                        ui->lcdNumber->display(value);
+                    }
+                } else if (reply->error() == QModbusDevice::ProtocolError) {
+                    qWarning() << tr("Read response error: %1 (Mobus exception: 0x%2)").arg(reply->errorString()).arg(reply->rawResult().exceptionCode(), -1, 16);
+                } else {
+                    qWarning() << tr("Read response error: %1 (code: 0x%2)").arg(reply->errorString()).arg(reply->error(), -1, 16);
+                }
+
+                reply->deleteLater();
+            });
+        } else {
+            delete reply; // broadcast replies return immediately
+        }
+    } else {
+        qWarning() << tr("Read error: ") + modbusDevice->errorString();
+    }
+}
--- S420_IHM/ihm.h
+++ S420_IHM/ihm.h
@@ -2,6 +2,9 @@
 #define IHM_H
 
 #include <QWidget>
+#include <QtWidgets>
+#include <QModbusClient>
+#include <QModbusDevice>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class IHM; }
@@ -10,10 +13,15 @@ QT_END_NAMESPACE
 class IHM : public QWidget
 {
     Q_OBJECT
-private:
-    Ui::IHM *ui;
+
 public:
     IHM(QWidget *parent = nullptr);
     ~IHM();
+private slots:
+    void sendValues();
+    void readValues();
+private:
+    Ui::IHM *ui;
+    QModbusClient *modbusDevice;
 };
 #endif // IHM_H
