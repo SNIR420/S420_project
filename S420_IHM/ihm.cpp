c7df5a63350dd704a0e715308815bbef587e56c6
SNIR420 <botnum57@gmail.com>
2023-02-17 08:39:17 +0100

    Refonte complète de l'IHM
    Modbus enlevé
    Ajout d'images pour les différents eléments
    Possibilité de saisir un angle et de l'envoyer depuis l'UI
    Changement de la position de l'icone de vent en modifiant l'angle

--- S420_IHM/ihm.cpp
+++ S420_IHM/ihm.cpp
@@ -1,93 +1,87 @@
 #include "ihm.h"
 #include "./ui_ihm.h"
 
-#include <QModbusDataUnit>
-#include <QModbusTcpClient>
-#include <QModbusReply>
-#include <QDebug>
-
 IHM::IHM(QWidget *parent): QWidget(parent), ui(new Ui::IHM)
 {
+    //modbus = new Modbus_SRV(this, ...)
+    //simulateur = new Simulateur(this, ...)
+
     ui->setupUi(this);
 
-    // Connecter le bouton pushButton avec la fonction sendValues
-    connect(ui->pushButton, &QPushButton::clicked, this, &IHM::sendValues);
-    connect(ui->pushButton_2, &QPushButton::clicked, this, &IHM::readValues);
-    // Initialiser le client Modbus
-    modbusDevice = new QModbusTcpClient(this);
-    modbusDevice->setConnectionParameter(QModbusDevice::NetworkAddressParameter, "127.0.0.1");
-    modbusDevice->setConnectionParameter(QModbusDevice::NetworkPortParameter, 502);
-    modbusDevice->setTimeout(1000);
-    modbusDevice->setNumberOfRetries(3);
-    modbusDevice->connectDevice();
+    //Image de fond
+    QPixmap pixmap("C:/Users/ligni/Desktop/S420_project/S420_IHM/images/background.png");
+    QBrush brush(pixmap);
+
+    QGraphicsRectItem *backgroundItem = new QGraphicsRectItem(ui->graphicsView->rect());
+    backgroundItem->setRect(ui->graphicsView->rect());
+    backgroundItem->setBrush(brush);
+
+    scene = new QGraphicsScene(ui->graphicsView->rect(), this);
+    scene->addItem(backgroundItem);
+
+    //Image Bateau
+    QPixmap centerImage("C:/Users/ligni/Desktop/S420_project/S420_IHM/images/boat.png");
+    QGraphicsPixmapItem *centerImageItem = new QGraphicsPixmapItem(centerImage);
+    centerImageItem->setPos((ui->graphicsView->width() - centerImage.width()) / 2, (ui->graphicsView->height() - centerImage.height()) / 2);
+    centerImageItem->setTransformOriginPoint(centerImage.width() / 2, centerImage.height() / 2); // Définir l'origine au centre de l'image
+    scene->addItem(centerImageItem);
+    ui->graphicsView->setScene(scene);
+    ui->graphicsView->setFixedSize(scene->width(), scene->height());
+
+    connect(ui->pushButton, &QPushButton::clicked, this, [=](){
+        setAngleVent(ui->doubleSpinBox->value());
+    });
+}
+
+
+void IHM::setAngleVent(double angleDeg){
+    //crée un qpixmap à partir de l'emplacement de l'image à utiliser
+    QPixmap topLeftImage("C:/Users/ligni/Desktop/S420_project/S420_IHM/images/wind.png");
+    QGraphicsPixmapItem *topLeftImageItem = nullptr;
+
+    // Vérifie que l'image n'est pas déjà présente sur la scène et la supprime si c'est le cas
+    QList<QGraphicsItem*> items = scene->items();
+    for (QGraphicsItem *item : items) {
+        if (item->type() == QGraphicsPixmapItem::Type && item->data(Qt::UserRole) == "topLeftImage") {
+            // If the item exists, remove it from the scene
+            topLeftImageItem = qgraphicsitem_cast<QGraphicsPixmapItem*>(item);
+            scene->removeItem(topLeftImageItem);
+            break;
+        }
+    }
+    //crée le nouvel item graphique associé à l'image
+    topLeftImageItem = new QGraphicsPixmapItem(topLeftImage);
+
+    // Définir le rayon et l'angle de rotation
+    double radius = 200.0;
+    double angleRad = qDegreesToRadians(angleDeg);
+
+    // Calcule les coordonées selon le cos/sin de l'angle en radian
+    double x, y = 250;
+
+    x = 250 - (radius * qCos(angleRad));
+    y = 250 - (radius * qSin(angleRad));
+
+    // Définir la position de l'image et la faire pivoter autour de l'origine de backgroundItem
+    topLeftImageItem->setPos(x+24, y+24);
+
+    //ajoute l'image à la scene
+    scene->addItem(topLeftImageItem);
+
+    //Change le centre de rotation de l'image pour qu'il pivote sur lui-même
+    topLeftImageItem->setTransformOriginPoint((topLeftImage.width()/2), (topLeftImage.height()/2)); // Définir l'origine de rotation au coin supérieur gauche de l'image
+
+    // Faire tourner l'image selon l'angle donné
+    topLeftImageItem->setRotation(angleDeg);
+
+    // Attribuer la donné topLeftImage pour identifier l'image lors de la création de la prochaine et pouvoir la supprimer
+    topLeftImageItem->setData(Qt::UserRole, "topLeftImage");
+
+    //Réactualiser la scène pour éviter les bug visuels
+    scene->update();
 }
 
 IHM::~IHM()
 {
     delete ui;
-}
-
-void IHM::sendValues()
-{
-    int value = ui->doubleSpinBox_2->value();
-
-    if (!modbusDevice)
-        return;
-
-    QModbusDataUnit writeUnit(QModbusDataUnit::HoldingRegisters, 16, 2);
-    writeUnit.setValue(0, value);
-
-    auto *reply = modbusDevice->sendWriteRequest(writeUnit, 1);
-
-    if (!reply) {
-        QMessageBox::critical(this, tr("Error"), tr("Write error: ") + modbusDevice->errorString());
-    } else {
-        if (!reply->isFinished()) {
-            connect(reply, &QModbusReply::finished, this, [this, reply]() {
-                if (reply->error() == QModbusDevice::ProtocolError) {
-                    QMessageBox::critical(this, tr("Error"), tr("Write error: ") + reply->errorString());
-                } else if (reply->error() != QModbusDevice::NoError) {
-                    QMessageBox::critical(this, tr("Error"), tr("Write error: ") + reply->errorString());
-                } else {
-                    QMessageBox::information(this, tr("Success"), tr("Write success"));
-                }
-
-                reply->deleteLater();
-            });
-        } else {
-            reply->deleteLater();
-        }
-    }
-}
-
-void IHM::readValues()
-{
-
-    if (!modbusDevice)
-        return;
-    QModbusDataUnit readUnit(QModbusDataUnit::HoldingRegisters, 16, 1);
-    auto reply = modbusDevice->sendReadRequest(readUnit, 1);
-    if (reply) {
-        if (!reply->isFinished()) {
-            connect(reply, &QModbusReply::finished, this, [this, reply]() {
-                if (reply->error() == QModbusDevice::NoError) {
-                    const auto registers = reply->result().values();
-                    if (registers.size() == 1) {
-                        const int value = static_cast<short>(registers[0]);
-                        ui->lcdNumber->display(value);
-                    }
-                } else if (reply->error() == QModbusDevice::ProtocolError) {
-                    qWarning() << tr("Read response error: %1 (Mobus exception: 0x%2)").arg(reply->errorString()).arg(reply->rawResult().exceptionCode(), -1, 16);
-                } else {
-                    qWarning() << tr("Read response error: %1 (code: 0x%2)").arg(reply->errorString()).arg(reply->error(), -1, 16);
-                }
-
-                reply->deleteLater();
-            });
-        } else {
-            delete reply; // broadcast replies return immediately
-        }
-    } else {
-        qWarning() << tr("Read error: ") + modbusDevice->errorString();
-    }
 }
