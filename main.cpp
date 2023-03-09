ea4828b8374597354f0d89fdc17a82fbc580c23c
SNIR420 <botnum57@gmail.com>
2023-03-09 09:10:12 +0100

    Redimensionnement de la fenêtre selon l'écran

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "ihm.h"
 
 #include <QApplication>
+#include <QScreen>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     IHM w;
     w.show();
+
+    QRect availableScreenGeometry = QGuiApplication::primaryScreen()->availableGeometry();
+    w.setGeometry(0, 0, availableScreenGeometry.width(), availableScreenGeometry.height());
+
     return a.exec();
 }
