fd2cdbbc2489979eee46cdcf60c40ad6465010f1
SNIR420 <botnum57@gmail.com>
2023-03-09 08:58:58 +0100

    Revert "Tableau double entrée"
    This reverts commit 50548f578a91cf017889765565d571f9462340f8.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
-#include <QtCore>
-#include "polaire.h"
+#include <QCoreApplication>
+#include <QLocale>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication app(argc, argv);
+    QCoreApplication a(argc, argv);
 
-    // Créer une variable Polaire
-    Polaire polaire("/home/projetbateau/Bureau/Class40.pol");
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "ProjetBateau_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
 
-    return app.exec();
+    return a.exec();
 }
--- polaire.h
+++ polaire.h
@@ -1,22 +1,17 @@
+
 #ifndef POLAIRE_H
 #define POLAIRE_H
 
 #include <QObject>
-#include <QFile>
-#include <QTextStream>
-#include <QDebug>
-#include <QRegularExpression>
 
 class Polaire : public QObject
 {
 private:
     Polaire(){}
-    QString m_polFilePath;
-    QVector<QVector<double>> m_polaireData;
     Q_OBJECT
 public:
     explicit Polaire(QObject *parent = nullptr);
-    Polaire(QString polFilePath);
+    Polaire(QObject polFilePath);
     double getSpeedRatio(double twa, double tws, double ratio);
     double getMaxSpeed(double twa, double tws);
     double getGiteRatio(double twa, double tws, double ratio);
