e10c961b93d42a1da11e67fbd4b42142725b42ec
SNIR420 <botnum57@gmail.com>
2023-03-17 16:20:00 +0100

    Modification classe Simulateur pour prendre en compte la vitesse de la vague

--- simulateur.cpp
+++ simulateur.cpp
@@ -3,8 +3,9 @@
 
 Simulateur::Simulateur(QString pol_location, Modbus_SRV* modbusserver, QObject *parent) : QObject(parent), m_modbusServer(modbusserver), m_polaire(pol_location) // Initialisation de m_modbusserver dans la liste d'initialisation du constructeur
 {
-    //création de l'object de type polaire
-    //connection modbus
+    m_x = 0 ;
+    m_y = 0 ;
+    m_angleAzimut = 0.785398163 ;
 
     QTimer *timer = new QTimer(this) ;
     connect(timer, &QTimer::timeout, this, &Simulateur::calcul) ;    //connect le timeout() du timer à une fonction qui calcule roulis, tangage et vitesse azimut
@@ -21,10 +22,10 @@ void Simulateur::setRoulis(){
     cout << "\tdeltaT : " << t0.secsTo(t1) << endl ;*/
 
     double ze = getVagueAmplitude() * sin(2.0*PI/getVaguePeriode() * (m_t0.msecsTo(m_t1)/1000.0)
-        - (2.0*PI / getInterVague()) * (Y + envergure/2.0) * sin(getAngleAzimut())) ;
+        - (2.0*PI / getInterVague()) * (cos(getAngleAzimut()) * m_speed * 1852.0 / 3600.0 * m_t0.msecsTo(m_t1)/1000.0 + envergure/2.0 * sin(getAngleAzimut()))) ;
 
     double zd = getVagueAmplitude() * sin(2.0*PI/getVaguePeriode() * (m_t0.msecsTo(m_t1)/1000.0)
-        - (2.0*PI / getInterVague()) * (Y - envergure/2.0) * sin(getAngleAzimut())) ;
+        - (2.0*PI / getInterVague()) * (cos(getAngleAzimut()) * m_speed * 1852.0 / 3600.0 * m_t0.msecsTo(m_t1)/1000.0 - envergure/2.0 * sin(getAngleAzimut()))) ;
 
     if ((ze - zd)/envergure <= 1 && (ze - zd)/envergure >= -1)
     m_roulis = asin((ze - zd)/envergure) ;
@@ -35,10 +36,10 @@ void Simulateur::setTangage(){
     m_t1 = m_t1.currentTime() ;
 
     double zc = getVagueAmplitude() * sin(2.0*PI/getVaguePeriode() * (m_t0.msecsTo(m_t1)/1000.0)
-        - (2.0*PI / getInterVague()) * (Y + Longueur/2.0) * cos(getAngleAzimut())) ;
+        - (2.0*PI / getInterVague()) * (cos(getAngleAzimut()) * m_speed * 1852.0 / 3600.0 * m_t0.msecsTo(m_t1)/1000.0 + Longueur/2.0 * cos(getAngleAzimut()))) ;
 
     double za = getVagueAmplitude() * sin(2.0*PI/getVaguePeriode() * (m_t0.msecsTo(m_t1)/1000.0)
-        - (2.0*PI / getInterVague()) * (Y - Longueur/2.0) * cos(getAngleAzimut())) ;
+        - (2.0*PI / getInterVague()) * (cos(getAngleAzimut()) * m_speed * 1852.0 / 3600.0 * m_t0.msecsTo(m_t1)/1000.0 - Longueur/2.0 * cos(getAngleAzimut()))) ;
 
     if ((zc - za)/Longueur <= 1 && (zc - za)/Longueur >= -1)
     m_tangage = asin((zc - za)/Longueur) ;
@@ -46,7 +47,7 @@ void Simulateur::setTangage(){
 }
 
 void Simulateur::setVitesseAzimut(){
-    m_vitesseAzimut = 0.0 ;
+
 }
 
 double Simulateur::getVagueAmplitude(){
@@ -57,24 +58,54 @@ double Simulateur::getVaguePeriode(){
     return vaguePeriode ;
 }
 
-/*double simulateur::getVagueVitesse(){
-    return 4.0 ;
-}*/
+double Simulateur::getVagueVitesse(){
+    if(m_modbusServer->getVitvague() == 0)  return 0.01;
+    else return m_modbusServer->getVitvague();
+}
+
+void Simulateur::setSpeed(){ // utilise la classe polaire pour obtenir la vitesse
+    int i = 0, j = 0 ;
+    while(polaire->getPolaireData()[0][i] <= getTws()){
+        i++ ;
+    }
+    while(polaire->getPolaireData()[j][0] <= getTwa()){
+        j++ ;
+    }
+    m_speed = polaire->getPolaireData()[j][i] ;
+    //m_speed = 0.0 ;
+}
 
 double Simulateur::getAngleAzimut(){
-    return angleAzimut ;
+    return m_angleAzimut ;
 }
 
 double Simulateur::getInterVague(){
-    return interVague ;
+    return getVaguePeriode()*getVagueVitesse() ;
+}
+
+double Simulateur::getTws(){
+    return TWS ;
+}
+
+double Simulateur::getTwa(){
+    return TWA ;
 }
 
 void Simulateur::calcul(){
+    m_x += sin(getAngleAzimut()) * m_speed * 1852 / 3600 * 0.1 ; // 1852 et 3600 pour conversion noeuds/mètres
+    m_y += cos(getAngleAzimut()) * m_speed * 1852 / 3600 * 0.1 ; // 0.1s soit l'intervalle du timer
     setRoulis();
     setTangage();
-    setVitesseAzimut();
-    m_modbusServer->setRoulis(round(m_roulis*180.0/PI));
-    m_modbusServer->setTangage(round(m_tangage*180.0/PI));
+    m_modbusServer->setRoulis(m_roulis*180.0/PI);
+    m_modbusServer->setTangage(m_tangage*180.0/PI);
+    setVitesseAzimut(); // inutile pour l'instant
+    setSpeed();
+    /*qDebug() << "roulis    : " << round(m_roulis*180.0/PI*10.0)/10.0 << "°";
+    qDebug() << "tangage   : " << round(m_tangage*180.0/PI*10.0)/10.0 << "°";
+    qDebug() << "vitesse   : " << m_speed << " nd";
+    qDebug() << "delta thé : " << round(m_t0.msecsTo(m_t1)/100.0)/10.0 << "s  \t" << m_t0.msecsTo(m_t1) << "ms";
+    qDebug() << "x = " << m_x << "  y = " << m_y;
+    qDebug() << "angle azimut : " << m_angleAzimut*180/PI << "°";*/
 }
 
 void Simulateur::setWave(int force){
--- simulateur.h
+++ simulateur.h
@@ -2,23 +2,26 @@
 #ifndef SIMULATEUR_H
 #define SIMULATEUR_H
 
-#include <QWidget>
-#include <QObject>
-#include <cmath>
 #include <QTimer>
 #include <QTime>
+#include <QObject>
+#include <cmath>
 #include "modbus_srv.h"
 #include "polaire.h"
 
 #define PI 3.14159256
-#define envergure 2.0
-#define Longueur 4.0
-#define Y 1.0
+#define envergure 2.0 // mètres
+#define Longueur 4.0  // mètres
+#define Y 0.0         // mètres
 
-#define vagueAmplitude 0.5
-#define vaguePeriode 10.0
-#define angleAzimut 0.785
-#define interVague 4.0
+#define vagueAmplitude 3.0 // mètres
+#define vaguePeriode 10.0  // secondes
+//#define angleAzimut 0.785398163
+
+//#define interVague 30
+
+#define TWS 0.1   //noeuds
+#define TWA 1.1  //degrés
 
 class Simulateur : public QObject
 {
@@ -28,7 +31,6 @@ private:
     double m_angleVentJeu ; // System Wind Angle
     double m_tws ;
     int m_beaufort ;  // de 0 à 12
-
     double m_roulis ;
     double m_tangage ;
     double m_vitesseAzimut ; // vitesse de rotation sur le lacet
@@ -39,6 +41,17 @@ private:
     Simulateur* simulateur;
     Polaire* polaire;
     QString m_polaire;
+
+    double m_x ;
+    double m_y ;
+    double m_speed ;
+
+    /*double m_vaguePeriode ;
+        double m_vagueVitesse ;*/
+    double m_angleAzimut ; // radians
+    // 0    correspond à un roulis nul     et à un tangage maximum
+    // pi/2 correspond à un roulis maximum et à un tangage nul (environ 1.57)
+    // pi/4 (environ 0.785398163)
 
 public:
     explicit Simulateur(QString pol_location, Modbus_SRV* modbusserver, QObject *parent = nullptr); // Ajout d'un paramètre de type pointeur vers Modbus_SRV
@@ -64,12 +77,9 @@ public:
     void setTangage() ;
     void setVitesseAzimut() ;
 
+    void setSpeed() ;
 private slots:
     void calcul() ;
-
-signals:
-    // déclaration des signaux utilisés pour communiquer avec l'IHM
-    void someSignal(int arg1, bool arg2);
 };
 
 #endif // SIMULATEUR_H
