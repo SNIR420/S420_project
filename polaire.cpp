50c49f296abc019d959020ceff8d5008f0d60139
SNIR420 <124249977+SNIR420@users.noreply.github.com>
2023-06-01 17:02:28 +0200

    Add files via upload

--- polaire.cpp
+++ polaire.cpp
@@ -29,21 +29,15 @@ Polaire::Polaire(QString polFilePath)
             m_polaireData.append(data);
         }
     }
-
     file.close();
-
-    // Afficher les données Polaire
-    for (int i = 0; i < m_polaireData.size(); ++i) {
-        QVector<double> row = m_polaireData[i];
-        for (int j = 0; j < row.size(); ++j) {
-            qDebug() << "m_polaireData[" << i << "][" << j << "] = " << row[j];
-        }
-    }
-
 }
+
 double Polaire::getMaxSpeed(double twa, double tws) {
     int x = -1, y = -1;
     int i, j, k = 0;
+
+    if (tws == 0 || twa == 0) return 0.0 ;
+    if (tws > 0.0 && tws < 4.0) tws = 4.0 ;
 
     for(i = 0; i < m_polaireData.size(); i++) {
         if(m_polaireData[i][0] == twa) { // Trouver la ligne correspondant à l'angle d'attaque (twa)
@@ -59,13 +53,13 @@ double Polaire::getMaxSpeed(double twa, double tws) {
         for(j = 0; m_polaireData[j][0] <= twa; j++);
         double x1 = m_polaireData[j][0];
         double x2 = m_polaireData[j-1][0];
-
         for(k = 0; m_polaireData[0][k] <= tws; k++);
         double y2 = m_polaireData[0][k];
         double y1 = m_polaireData[0][k-1];
 
-        qDebug() << "x1: "<< x1 << "x2: " << x2 << "y1: " << y1 << "y2: " << y2 ;
+        //qDebug() << "x1: "<< x1 << "x2: " << x2 << "y1: " << y1 << "y2: " << y2 ;
         double test = (getMaxSpeed(x2, y1) - getMaxSpeed(x1, y1)) * (twa-x1)/ (x2-x1) + (getMaxSpeed(x1, y2) - getMaxSpeed(x1, y1)) * (tws-y1)/(y2-y1) + (getMaxSpeed(x2, y2) + getMaxSpeed(x1, y1) - getMaxSpeed(x2, y1) - getMaxSpeed(x1, y2)) * (twa-x1)/(x2-x1) * (tws-y1)/(y2-y1) + getMaxSpeed(x1, y1);
+
 
         return test;
     }
@@ -74,12 +68,11 @@ double Polaire::getMaxSpeed(double twa, double tws) {
 }
 
 double Polaire::getMaxRatio(double twa, double tws) {
-    qDebug() << getMaxSpeed(twa, tws) * 0.7;
+    //qDebug() << getMaxSpeed(twa, tws) * 0.7;
     return getMaxSpeed(twa, tws) * 0.7;
 }
 
 double Polaire::getSpeedRatio(double twa, double tws, double ratio){
     //qDebug() << getMaxSpeed(twa, tws) * ratio ;
     return getMaxSpeed(twa, tws) * ratio ;
-
 }
